4603d445825a842310fc53eded076ac95dea64b1
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-16 16:54:44 +0300

    Aritmetik

---
+++ src/patika/aritmetik/Main.java
@@ -0,0 +1,24 @@
+package patika.aritmetik;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("3 adet sayı giriniz.");
+        int numberOne = input.nextInt();
+        int numberTwo = input.nextInt();
+        int numberThree = input.nextInt();
+
+        int result = result(numberOne, numberTwo, numberThree);
+        System.out.println("Result: " + result);
+    }
+
+    private static int result(int numberOne, int numberTwo, int numberThree)
+    {
+        int result = numberOne + (numberTwo * numberThree) - numberTwo;
+        return result;
+    }
+}
