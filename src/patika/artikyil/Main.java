9b5db7057b3488e80899d85ca8e59b97a06e9256
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-16 19:03:25 +0300

    Changes

--- src/patika/artikyil/Main.java
+++ src/patika/artikyil/Main.java
@@ -28,8 +28,16 @@ public class Main
         Scanner input =  new Scanner(System.in);
         System.out.println("Yıl Giriniz: ");
         int year = input.nextInt();
-        leapYearCalculator(year);
+        boolean leapYear = leapYearCalculator(year);
 
+        if (leapYear)
+        {
+            System.out.println(year + " artık yıldır.");
+        }
+        else
+        {
+            System.out.println(year + " artık yıl değildir.");
+        }
     }
 
     /**
@@ -37,15 +45,8 @@ public class Main
      *
      * @param year Kontrol edilecek yıl
      * */
-    private static void leapYearCalculator(int year)
+    private static boolean leapYearCalculator(int year)
     {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-        {
-            System.out.println(year + " bir artık yıldır !");
-        }
-        else
-        {
-            System.out.println(year + " bir artık yıl değildir !");
-        }
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
