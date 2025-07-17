0de32d29c01fb6e64a4af2eefe542a152bf7dd3c
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-17 11:30:27 +0300

    Full Triangle

--- src/patikaweek1/tersucgen/Main.java
+++ src/patikaweek1/tersucgen/Main.java
@@ -27,11 +27,15 @@ public class Main
     {
         for (int i = 0; i <= stepNumber; i++)
         {
-            for (int j = stepNumber; j >= i; j--)
+            for (int j = 0; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * (stepNumber - i) - 1); k++)
             {
                 System.out.print("*");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
