f1ba165191686bfa2ee9282cf959d33cb35663f3
Plousieleex <a-xy.cnbrk@hotmail.com>
2025-07-24 23:49:11 +0300

    Map

---
+++ src/patikaweek3/mapodevi/Main.java
@@ -0,0 +1,35 @@
+package patikaweek3.mapodevi;
+
+import java.util.HashMap;
+
+/**
+ * <p>
+ *     Uygulama, oluşturulan HashMap içerisinde giriş yapılmış İsim (Key) ve
+ *     not (Value) değerlerini sırayla döndürür.
+ * </p>
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        HashMap<String, Integer> scoresMap = new HashMap<>();
+        scoresMap.put("Ali", 85);
+        scoresMap.put("Ayşe", 92);
+        scoresMap.put("Mehmet", 78);
+        scoresMap.put("Zeynep", 90);
+        scoresMap.put("Burak", 88);
+
+        printMap(scoresMap);
+    }
+
+    /**
+     * Kullanıcının oluşturduğu mapin içerisindeki key, value değerlerini
+     * döndürür.
+     * @param map Kullanıcının oluşturduğu HashMap
+     */
+    private static void printMap(HashMap<String, Integer> map)
+    {
+        map.forEach((key, value) ->
+                System.out.println(key + " = " + value));
+    }
+}
